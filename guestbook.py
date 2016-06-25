019418ee652332d41369f406bc38e09552fe190e
tristan-lin <tristan-lin@users.noreply.github.com>
2016-06-25 21:33:47 +0900

    trial one
    only filter myself name

--- guestbook.py
+++ guestbook.py
@@ -72,21 +72,22 @@ class MainPage(webapp2.RequestHandler):
         greetings = greetings_query.fetch(10)
 
         user = users.get_current_user()
-        if user:
+        if user == "linxinan@gmail.com":
             url = users.create_logout_url(self.request.uri)
             url_linktext = 'Logout'
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
-
-        template_values = {
+            template_values = {
             'user': user,
             'greetings': greetings,
             'guestbook_name': urllib.quote_plus(guestbook_name),
             'url': url,
             'url_linktext': url_linktext,
         }
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+            template_values = {}
 
+        
         template = JINJA_ENVIRONMENT.get_template('index.html')
         self.response.write(template.render(template_values))
 # [END main_page]
